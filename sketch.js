5a46e9bfb5f82a404c961e6547091c082df865fd
theking2351 <76833292+theking2351@users.noreply.github.com>
2021-02-06 13:19:02 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,29 +1,61 @@
-var movingRect,fixedRect ;
+var cat ,catRunning,catImg1,catlastImage
+var mouse,mouseTeasing,mouseImg1,mouselastImage;
+background,backgroundImg;
+function preload() {
+    //load the images here
+   backgroungImg=loadImage("garden")
+    catImg1=loadImage("cat1");
+    catlastImage.loadAnimation("cat4");
+    mouseImg1=loadImage("mose1");
+    mouseLastImage.loadAnimationdImage("mose4");
+    catRunning.loadAnimation("cat2,cat3");
+    mouseTeasing.loadAnimation("mouse2,mouse3");
 
-function setup() {
-  createCanvas(800,400);
-  movingRect =createSprite(400, 200, 50, 50);
-  movingRect.shapeColor="green";
-  fixedRect=createSprite(200,200,50,50);
-  fixedRect.shapeColor="green";
 }
 
+function setup(){
+    createCanvas(1000,800);
+background=createSprite(500,500,500,300);
+background.addImage(backgroundImg)
+
+cat =createSprite(600,700,50,60);
+cat.addImage(catImg1);
+
+mouse=createSprite(200,700,50,60)
+mouse.addImage(mouseImg1);
+}
 
 function draw() {
-  background(0);
-  movingRect.x=mouseX;
-  movingRect.y=mouseY;
 
-if (movingRect.x-fixedRect.x<=movingRect.width/2+fixedRect.width/2&&
-  fixedRect.x-movingRect.x<=movingRect.width/2+fixedRect.width/2&&
-  movingRect.y-fixedRect.y<=movingRect.height/2+fixedRect.height/2&&
-  fixedRect.y-movingRect.y<=movingRect.height/2+fixedRect.height/2){
-  movingRect.shapeColor="red";
-  fixedRect.shapeColor="red";
-}else{
-  movingRect.shapeColor="green";
-  fixedRect.shapeColor="green"
+    background(255);
+     keyPressed();
+
+    //Write condition here to evalute if tom and jerry collide
+     if (cat.x-mouse.x<(cat.width-mouse.width)/2) {
+         cat.velocityX=0;
+        cat.addAnimation("moseLastImage",mouseImg4)
+        cat.changeAnimation("mouselastImage")  
+       
+       
+        mouse.addAnimation("mouseLastImage",catImg4)
+        cat.changeAnimation("catlastImage")  
+
+
+     }
+    drawSprites();
 }
 
-  drawSprites();
+
+function keyPressed(){
+
+  //For moving and changing animation write code here
+if (keyCode===LEFT_ARROW) {
+    cat.velocityX=-5;
+    cat.addAnimation("catRunning");
+    cat.changeAnimation("catRunning")
+
+    mouse.addAnimation("moseTeasing");
+    mouse.changeAnimation("mouseTeasing");
+}
+
 }
